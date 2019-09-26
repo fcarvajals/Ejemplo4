79ef0dbec19bdc08c58d86abc8bdda597ec24f37
Fernelly Carvajal <fcarvajals@misena.edu.co>
2019-09-26 14:36:17 -0500

    Última modificación 26-09

--- scripts.js
+++ scripts.js
@@ -1,21 +1,29 @@
-var request=new XMLHttpRequest();
-
-request.open('GET','https://ghibliapi.herokuapp.com/films', true);
-
-request.onload=function(){
-    var data=JSON.parse(this.response);
-    data.array.forEach(movie => {
-        console.log(movie.title);
-    });
-}
-request.send();
+//Se incluye LOGO y Contenedor
 const app=document.getElementById('root');
 const logo=document.createElement('img');
-logo.src='logo.png';
-
+logo.src='logo.jpg';
 const container=document.createElement('div');
-container.setAttribute('class','container');
+container.setAttribute('class', 'container');
 app.appendChild(logo);
 app.appendChild(container);
 
+//Se comienza a pedir datos
+var request=new XMLHttpRequest();
+request.open('GET','https://ghibliapi.herokuapp.com/films',true);  
+request.onload=function() {
+var data=JSON.parse(this.response);
+data.forEach(movie=>{
+    const card=document.createElement('div');
+    card.setAttribute('class', 'card');
+    const h1=document.createElement('h1');
+    h1.textContent=movie.title;
+    const p=document.createElement('p');
+    movie.description=movie.description.substring(0, 300); // Limit to 300 chars
+    p.textContent=movie.description;
+    container.appendChild(card);
+    card.appendChild(h1);
+    card.appendChild(p);
+})
+}
+request.send();
 
